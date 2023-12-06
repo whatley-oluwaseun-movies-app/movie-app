9c7ebf93cb9acd72c1cbf9fb86ed54b0903c6c56
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-12-06 11:36:35 -0600

    completed the frame work of project and some java script

--- js/movieApp.js
+++ js/movieApp.js
@@ -1,109 +1,53 @@
-// movieApp.js
-document.addEventListener("DOMContentLoaded", () => {
-    const loadingElement = document.getElementById("loading");
-    const moviesListElement = document.getElementById("movies-list");
-    const addMovieForm = document.getElementById("add-movie-form");
+'use strict';
 
-    // Function to fetch movies
-    const fetchMovies = async () => {
-        try {
-            const response = await fetch("http://localhost:3000/movies");
-            const movies = await response.json();
-            return movies;
-        } catch (error) {
-            console.error("Error fetching movies:", error);
-        }
-    };
+/**
+ * navbar variables
+ */
 
-    // Function to render movies
-    const renderMovies = (movies) => {
-        moviesListElement.innerHTML = "";
-        movies.forEach((movie) => {
-            const listItem = document.createElement("li");
-            listItem.innerHTML = `<span>${movie.title} - Rating: ${movie.rating}</span> 
-                           <button onclick="editMovie(${movie.id})">Edit</button>
-                           <button onclick="deleteMovie(${movie.id})">Delete</button>`;
-            moviesListElement.appendChild(listItem);
-        });
-    };
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
 
-    // Function to handle form submission
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
-        const title = document.getElementById("title").value;
-        const rating = document.getElementById("rating").value;
+for (let i = 0; i < navElemArr.length; i++) {
 
-        try {
-            const response = await fetch("http://localhost:3000/movies", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ title, rating }),
-            });
+    navElemArr[i].addEventListener("click", function () {
 
-            const newMovie = await response.json();
-            // Fetch and render updated movie list
-            const movies = await fetchMovies();
-            renderMovies(movies);
-        } catch (error) {
-            console.error("Error adding movie:", error);
-        }
-    };
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+        document.body.classList.toggle("active");
 
-    // Function to handle movie deletion
-    window.deleteMovie = async (movieId) => {
-        try {
-            await fetch(`http://localhost:3000/movies/${movieId}`, {
-                method: "DELETE",
-            });
+    });
 
-            // Fetch and render updated movie list
-            const movies = await fetchMovies();
-            renderMovies(movies);
-        } catch (error) {
-            console.error("Error deleting movie:", error);
-        }
-    };
+}
 
-    // Function to handle movie editing
-    window.editMovie = async (movieId) => {
-        // Fetch movie details
-        try {
-            const response = await fetch(`http://localhost:3000/movies/${movieId}`);
-            const movieDetails = await response.json();
 
-            // Pre-populate the form with movie details
-            document.getElementById("title").value = movieDetails.title;
-            document.getElementById("rating").value = movieDetails.rating;
 
-            // TODO: Add logic to update movie details on form submission
-        } catch (error) {
-            console.error("Error fetching movie details:", error);
-        }
-    };
+/**
+ * header sticky
+ */
 
-    // Initial setup
-    const init = async () => {
-        try {
-            // Start JSON server
-            await fetch("http://localhost:3000/movies");
-            // Fetch movies and render
-            const movies = await fetchMovies();
-            renderMovies(movies);
-            // Remove loading message
-            loadingElement.style.display = "none";
-        } catch (error) {
-            console.error("Error initializing app:", error);
-        }
-    };
+const header = document.querySelector("[data-header]");
 
-    // Event listeners
-    addMovieForm.addEventListener("submit", handleFormSubmit);
+window.addEventListener("scroll", function () {
 
-    // Initialize app
-    init();
+    window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
 });
 
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+    window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+
+});
 
