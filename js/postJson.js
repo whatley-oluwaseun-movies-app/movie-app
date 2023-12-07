2a7fdf16138c9dafbbec40f3d34eb55315579079
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-12-07 16:25:42 -0600

    updated

--- js/postJson.js
+++ js/postJson.js
@@ -1,117 +1,107 @@
-// ADDED A COUNTER TO ALLOW INCREMENTING OF ID
-let counter = 15;
+let counter = 100; // Starting counter from a higher value to avoid potential conflicts
+let movies = []; // Empty array, will be populated with data from JSON file
+let displayedMovies = 4; // Number of movies to display initially
 
-function renderCoffee(coffee) {
-    let html = '<div class="coffee row col-6">';
-    // ADDED COUNTER TO INCREMENT ON NEW COFFEES, OR TO CALL EXISTING ID'S ON CURRENT COFFEES
-    html += `<p id="coffee-id">${coffee.id || counter++}</p>`;
-    html += `<p class="col-7">${coffee.name}</p>`;
-    html += `<p class="col-5">${coffee.roast}</p>`; //p tag
-    html += '</div>'; //div
-
+function renderMovie(movie) {
+    let html = '<div class="movie row col-6">';
+    html += `<img class="col-12" src="${movie.image}" alt="${movie.title}">`; // Include the image property
+    // html += `<p id="movie-id">${movie.id || counter++}</p>`;
+    // html += `<h5 class="col-7">${movie.title}</h5>`;
+    html += `<h5 class="col-2">${movie.rating}</h5>`;
+    html += `<p class="col-12">${movie.movieSummary}</p>`;
+    html += `<p class="col-12">${movie.genre}</p>`;
+    html += `<button class="btn btn-danger btn-sm" onclick="deleteMovie(${movie.id})">Delete</button>`;
+    html += `<button class="btn btn-warning btn-sm" onclick="editMovie(${movie.id})">Edit</button>`;
+    html += '</div>';
     return html;
 }
 
-function renderCoffees(coffees) {
+function renderMovies(movies, limit) {
     let html = '';
-    for (let i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    for (let i = movies.length - 1; i >= 0 && limit > 0; i--) {
+        html += renderMovie(movies[i]);
+        limit--;
     }
     return html;
 }
 
-
-
-// CHECKING IF AN OPTION IS ALL ROASTS, IF SO WILL DISPLAY ALL ROASTS
-// ADDED AN IF STATEMENT FOR CHECKING FOR ALL ROASTS FIRST THEN CONTINUE WITH THE FUNCTION
-// NOT SURE IF THERE'S A NICER WAY TO SHOW THIS
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    const selectedRoast = roastSelection.value;
-    if (selectedRoast === "All Roasts") {
-        section.innerHTML = renderCoffees(coffees);
+function updateMovies(e) {
+    e.preventDefault();
+    const selectedGenre = roastSelection.value;
+    if (selectedGenre === "All Genre") {
+        section.innerHTML = renderMovies(movies, displayedMovies);
     } else {
-        const filteredCoffees = [];
-        coffees.forEach(coffee => {
-            if (coffee.roast === selectedRoast) {
-                filteredCoffees.push(coffee);
-                section.innerHTML = renderCoffees(filteredCoffees);
-            }
-        });
-        section.innerHTML = renderCoffees(filteredCoffees);
-
+        const filteredMovies = movies.filter(movie => movie.genre.includes(selectedGenre));
+        section.innerHTML = renderMovies(filteredMovies, displayedMovies);
     }
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-const coffees = [
-    {id: 1, name: 'Light City', roast: 'Light'},
-    {id: 2, name: 'Half City', roast: 'Light'},
-    {id: 3, name: 'Cinnamon', roast: 'Light'},
-    {id: 4, name: 'City', roast: 'Medium'},
-    {id: 5, name: 'American', roast: 'Medium'},
-    {id: 6, name: 'Breakfast', roast: 'Medium'},
-    {id: 7, name: 'High', roast: 'Dark'},
-    {id: 8, name: 'Continental', roast: 'Dark'},
-    {id: 9, name: 'New Orleans', roast: 'Dark'},
-    {id: 10, name: 'European', roast: 'Dark'},
-    {id: 11, name: 'Espresso', roast: 'Dark'},
-    {id: 12, name: 'Viennese', roast: 'Dark'},
-    {id: 13, name: 'Italian', roast: 'Dark'},
-    {id: 14, name: 'French', roast: 'Dark'},
-];
-
-// SEARCH BAR CODE TO MAKE IT CASE INSENSITIVE WHEN LOOKING FOR A PARTICULAR BREW
-
-const searchBar = document.getElementById("coffee-name");
-
-searchBar.addEventListener("input", function () {
-    const searchTerm = this.value.toLowerCase();
-    const resultsList = coffees.filter(coffee => coffee.name.toLowerCase().includes(searchTerm));
-
-    section.innerHTML = renderCoffees(resultsList);
-});
-
-
-// Function to add a new coffee
-// FUNCTION IS CREATING A NEW ELEMENT AND APPENDING TO HTML, NEEDS TO PUSH NEW COFFEE TO EXISTING COFFEES LIST
-function addNewCoffee() {
-    // Get input values
-    const newCoffeeName = document.getElementById('newCoffeeName').value;
-    const newCoffeeRoast = document.getElementById('newCoffeeRoast').value;
-
-    // Validate input values
-    if (newCoffeeName === "" || newCoffeeRoast === "") {
-        alert("Please enter both coffee name and roast type.");
-        return;
+function deleteMovie(id) {
+    const index = movies.findIndex(movie => movie.id === id);
+    if (index !== -1) {
+        movies.splice(index, 1);
+        section.innerHTML = renderMovies(movies, displayedMovies);
     }
-
-    // Create a new coffee object
-    const newCoffee = {
-        id: counter++,
-        name: newCoffeeName,
-        roast: newCoffeeRoast
-    };
-
-    // Add the new coffee to the coffees array
-    coffees.push(newCoffee);
-
-    // Render the updated list of coffees
-    section.innerHTML = renderCoffees(coffees);
-
-    // Clear input fields
-    document.getElementById('newCoffeeName').value = "";
-    document.getElementById('newCoffeeRoast').value = "";
-
 }
 
+function editMovie(id) {
+    const movieToEdit = movies.find(movie => movie.id === id);
+    if (movieToEdit) {
+        const newName = prompt("Enter the new movie name:", movieToEdit.title);
+        const newRating = prompt("Enter the new movie rating:", movieToEdit.rating);
+
+        if (newName !== null && newRating !== null) {
+            movieToEdit.title = newName;
+            movieToEdit.rating = newRating;
+            section.innerHTML = renderMovies(movies, displayedMovies);
+        }
+    }
+}
 
 const section = document.querySelector('#coffees');
 const submitButton = document.querySelector('#submit');
 const roastSelection = document.querySelector('#roast-selection');
 
-section.innerHTML = renderCoffees(coffees);
+fetch('./data/movies.json')
+    .then(response => response.json())
+    .then(data => {
+        movies = data.movies;
+        section.innerHTML = renderMovies(movies, displayedMovies);
+    })
+    .catch(error => console.error('Error fetching data:', error));
 
-submitButton.addEventListener('click', updateCoffees);
+const searchBar = document.getElementById("coffee-name");
 
-//============================================================//
+searchBar.addEventListener("input", function () {
+    const searchTerm = this.value.toLowerCase();
+    const resultsList = movies.filter(movie => movie.title.toLowerCase().includes(searchTerm));
+    section.innerHTML = renderMovies(resultsList, displayedMovies);
+});
+
+function addNewMovie() {
+    const newMovieName = document.getElementById('newCoffeeName').value;
+    const newMovieGenre = document.getElementById('newCoffeeRoast').value;
+
+    if (newMovieName === "" || newMovieGenre === "") {
+        alert("Please enter both movie name and genre.");
+        return;
+    }
+
+    const newMovie = {
+        id: counter++,
+        title: newMovieName,
+        rating: "N/A", // You may want to prompt the user for the rating or set it to a default value
+        genre: newMovieGenre
+    };
+
+    movies.push(newMovie);
+    section.innerHTML = renderMovies(movies, displayedMovies);
+
+    document.getElementById('newCoffeeName').value = "";
+    document.getElementById('newCoffeeRoast').value = "";
+}
+
+section.innerHTML = renderMovies(movies, displayedMovies);
+
+submitButton.addEventListener('click', updateMovies);
+document.querySelector('#addCoffeeForm').addEventListener('submit', addNewMovie);
